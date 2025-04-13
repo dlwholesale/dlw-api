06238a2d4df1b236230836465dc4e5017409f57f
Reynier <reynierp.salas@gmail.com>
2025-04-13 14:20:40 -0400

    Updates

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true,
+    secure: process.env.SMTP_SECURE === 'true',
+    requireTLS: process.env.SMTP_REQUIRE_TLS === 'true',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
